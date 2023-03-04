b105d8fce758b25795f75d68d21e4c72a3f000dd
Adrian Koh <koh.adrian.2019@gmail.com>
2023-03-04 16:47:06 +0800

    feat: added toastify

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,3 @@
 export { Header as default } from './header/Header';
-export { ShowToast } from './showToast/ShowToats';
+export { errorToast } from './showToast/ShowToats';
 export { Spinner } from './spinner/Spinner';
--- src/components/showToast/ShowToats.tsx
+++ src/components/showToast/ShowToats.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
+import { toast } from 'react-toastify';
+import { SerializedError } from '@reduxjs/toolkit';
+import { TError } from '@store/stateDataTypes';
 
-interface IShowToast {
-  title: string;
-  msg: string;
-}
+export const isError = (e: any): boolean => !!(typeof e === 'object' && e.message && typeof e.message === 'string');
 
-export const ShowToast = ({ title, msg }: IShowToast) => {
-  return (
-    <div aria-live="polite" aria-atomic="true" style={{ position: 'relative', minHeight: 200 }}>
-      <div className="toast" style={{ position: 'absolute', top: 0, right: 0 }}>
-        <div className="toast-header">
-          <img src="..." className="rounded mr-2" alt="..." />
-          <strong className="mr-auto">{title}</strong>
+export const errorToast = (e: TError) => {
+  let displayMsg = '';
+  // check msg type
+  if (typeof e === 'string' && e) {
+    displayMsg = e;
+  } else if (isError(e)) {
+    // error object
+    displayMsg = (e as Error | SerializedError).message ?? '';
+  }
 
-          <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div className="toast-body">{msg}</div>
-      </div>
-    </div>
-  );
+  return toast.error(displayMsg);
 };
