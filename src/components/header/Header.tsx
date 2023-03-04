c98eecdbbe37daa17bb72b431c7b42b47ef17df2
Adrian Koh <koh.adrian.2019@gmail.com>
2023-03-04 16:46:01 +0800

    refactor: add search menu and use react-bootstrap

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,40 +1,16 @@
 import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 export const Header = () => {
   return (
-    <nav className="navbar navbar-expand-sm navbar-light bg-light">
-      <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/album">
-                Album
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/search">
-                Search
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/album">Album</Nav.Link>
+          <Nav.Link href="/search">Search</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 };
